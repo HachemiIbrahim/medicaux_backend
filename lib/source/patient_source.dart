9c067bd589eb41a0fcb3255e56085c660fb70b77
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2023-11-30 22:48:44 +0100

    patitient source

---
+++ lib/source/patient_source.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:medicaux_backend/database/sql_client.dart';
+import 'package:medicaux_backend/models/patient.dart';
+
+/// data source form MySQL
+
+class PatientSource {
+  /// initializing
+  const PatientSource(
+    this.sqlClient,
+  );
+
+  ///Fetches all table fields from list table in our database
+  Future<List<PatientModel>> fetchFields() async {
+    // sqlQuey
+    const sqlQuery = 'SELECT patientId,name,age,phone_number FROM patient;';
+    final result = await sqlClient.execute(sqlQuery);
+    final lists = <PatientModel>[];
+    for (final row in result.rows) {
+      lists.add(PatientModel.fromRowAssoc(row.assoc()));
+    }
+    return lists;
+  }
+
+  ///add a list to the database
+  Future<void> addList(String name, int age, String phoneNumber) async {
+    final sqlQuery =
+        'INSERT INTO patient (name,age,phone_number) VALUES ("$name",$age,"$phoneNumber");';
+    await sqlClient.execute(sqlQuery);
+  }
+
+  ///update the list
+  Future<void> updateList(
+      String name, int age, String phoneNumber, String id) async {
+    final sqlQuery =
+        'UPDATE patient SET name = "$name" , age = $age , phone_number = "$phoneNumber" where id = $id;';
+    await sqlClient.execute(sqlQuery);
+  }
+
+  ///delete a list
+  Future<void> deleteList(String id) async {
+    final sqlQuery = 'DELETE FROM patient WHERE id = $id;';
+    await sqlClient.execute(sqlQuery);
+  }
+
+  /// accessing you client
+  final MySQLClient sqlClient;
+}
